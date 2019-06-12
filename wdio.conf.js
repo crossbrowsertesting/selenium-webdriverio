fab884c6bb702264effc2a7da9cdc23f4a0a6d00
Andrew Elick <andrew.elick@SB-TN-T6A2JG5J.local>
2019-06-12 11:18:56 -0500

    Edited record network

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,50 +1,51 @@
 exports.config = {
     runner: 'local',
-   
+
     hostname: 'hub.crossbrowsertesting.com',
     port: 80,
-   
+
     services: ['crossbrowsertesting'],
     user: process.env.CBT_USERNAME,
     key: process.env.CBT_AUTHKEY,
     cbtTunnel: false, //set to true if a local connection is needed
-    
+
     specs: [
         './test/specs/**/*.js'
     ],
     exclude: [
         // 'path/to/excluded/files'
     ],
- 
+
     maxInstances: 10,
-    
+
     capabilities: [{
         maxInstances: 5,
-        
+
         platform: 'Windows',
         browserName: 'firefox',
-        record_video: 'true'
+        record_video: 'true',
+        record_network: 'false'
     }],
-  
+
     logLevel: 'info',
-    
+
     bail: 0,
-    
+
     baseUrl: 'http://localhost',
-    
+
     waitforTimeout: 10000,
 
     connectionRetryTimeout: 90000,
-    
+
     connectionRetryCount: 3,
 
     framework: 'mocha',
 
     reporters: ['spec'],
-    
-  
+
+
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
-    } 
+    }
 }
